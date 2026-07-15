aeb8201c4c3463e84bd8eba275a1cb620e156190
Spartacus1616 <s.s.shayantan@gmail.com>
2026-07-15 15:08:22 +0530

    area and circumference of the circle

---
+++ Python/004_area_and_circumference_of_circle.py
@@ -0,0 +1,8 @@
+r = int(input("Enter radius of the circle: "))
+
+area = 3.14 * r * r
+circumference = 3.14 * 2 * r
+
+print("Area of circle is ",area)
+
+print("Circumference of the circle is ",circumference)
