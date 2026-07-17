00aff046efc39693af567437f0130b9f0ceac275
Spartacus1616 <prabhav.1616@gmail.com>
2026-07-17 14:31:17 +0530

    moving ahead

---
+++ Python/012_check_a_number.py
@@ -0,0 +1,10 @@
+num = int(input(" Enter any number "))
+
+if num > 0 :
+    print("The number is positive ")
+
+if num < 0 :
+    print("The number is negative ")
+
+if num == 0 :
+    print("the number equals zero ")
---
+++ Python/013_the_greatest_number.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number "))
+b = int(input("Enter the second number "))
+c = int(input("Enter the third number "))
+
+if a > b and a > c :
+    print( a, " is the greatest number  ")
+if b > a and b > c :
+    print(b," is the greatest number ")
+if c > a and c > b :
+    print(c, " is the greatest number ")
+    
+# will it run if input is (5,5,3)
